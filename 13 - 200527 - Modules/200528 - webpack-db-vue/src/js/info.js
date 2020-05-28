2eca45eaf2f1b9db528692933060b908129ea755
Fe_lyh <599768790@qq.com>
2020-05-28 21:39:43 +0800

    webpack打包vue

---
+++ 13 - 200527 - Modules/200528 - webpack-db-vue/src/js/info.js
@@ -0,0 +1,9 @@
+export const name = "why";
+export const age = 18;
+export const height = 1.88;
+
+export function GetText(arr) {
+  arr.forEach((item) => {
+    console.log(`${item}--es6`);
+  });
+}
