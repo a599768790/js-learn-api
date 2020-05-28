ac39fdf60d3249e45b2102bf0a2eb4f6f61126c7
Fe_lyh <599768790@qq.com>
2020-05-28 15:08:23 +0800

    webpack打包scss和图片

---
+++ 13 - 200527 - Modules/200528 - webpack打包scss、jpg/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundel.js",
+    publicPath: "dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader", // 将 JS 字符串生成为 style 节点
+          },
+          {
+            loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
+          },
+          {
+            loader: "sass-loader", // 将 Sass 编译成 CSS
+          },
+        ],
+      },
+    ],
+  },
+};
